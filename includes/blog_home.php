1e4a6e872a0ae148b313ba60dcac46b737fb19ac
Tom <tom@Toms-iMac.local>
2018-10-15 07:02:31 +0200

    Propreté SCSS + début page post_home

---
+++ includes/blog_home.php
@@ -0,0 +1,34 @@
+<section id="home">
+
+  <div id="left">
+    <div>
+      <h2><i class="fab fa-hotjar"></i>  Derniers articles</h2>
+      <div class="post_list">
+        <a href="<?php echo $GLOBALS['url']; ?>" class="post_list_item">
+          <img src="https://via.placeholder.com/100x100" alt="" />
+          <div><h4>Intelligence artificielle partie 2</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+        </a>
+        <a href="<?php echo $GLOBALS['url']; ?>" class="post_list_item">
+          <img src="https://via.placeholder.com/100x100" alt="" />
+          <div><h4>Intelligence artificielle partie 3</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+        </a>
+        <a href="<?php echo $GLOBALS['url']; ?>" class="post_list_item">
+          <img src="https://via.placeholder.com/100x100" alt="" />
+          <div><h4>Journal piscine 42 : jour 3/30</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div id="right">
+    <div id="categories">
+      <h2><i class="fas fa-list-ul"></i>  Catégories</h2>
+      <a href="/test/"># IA</a>
+      <a href="/test/"># 42</a>
+      <a href="/test/"># Développement Web</a>
+    </div>
+  </div>
+
+</section>
--- includes/header.php
+++ includes/header.php
@@ -3,8 +3,8 @@
   <a href="<?php echo $GLOBALS['url']; ?>"><h2>Tom Marx</h2></a>
   <div class="nav" id="nav" open="false">
     <i id="icon_close" class="fas fa-times" onclick="document.getElementById('nav').setAttribute('open', 'false')"></i>
-    <a href="<?php echo $GLOBALS['url']; ?>blog/">Blog</a>
-    <a href="<?php echo $GLOBALS['url']; ?>cv/">Mon CV</a>
-    <a href="<?php echo $GLOBALS['url']; ?>contact/">Contact</a>
+    <a href="<?php echo $GLOBALS['url']; ?>blog/" class="underline">Blog</a>
+    <a href="<?php echo $GLOBALS['url']; ?>cv/" class="underline">Mon CV</a>
+    <a href="<?php echo $GLOBALS['url']; ?>contact/" class="underline">Contact</a>
   </div>
 </header>
