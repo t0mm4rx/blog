6b914edd37f636715402b7a7c6cbdfe4c010962e
Tom <tom@Toms-iMac.local>
2018-10-24 11:31:29 +0200

    CV v1

--- fonctions.php
+++ fonctions.php
@@ -4,7 +4,7 @@ $GLOBALS['prod'] = false;
 if ($_SERVER['REMOTE_ADDR'] == "127.0.0.1" || $_SERVER['REMOTE_ADDR'] == "::1") {
     $GLOBALS["url"] = "http://localhost/blog/";
 } else if (strpos($_SERVER['REMOTE_ADDR'], "192.168") !== false) {
-    $GLOBALS["url"] = "http://192.168.0.7/blog/";
+    $GLOBALS["url"] = "http://192.168.0.6/blog/";
 } else {
     $GLOBALS["url"] = "https://tommarx.free.fr/";
     $GLOBALS['prod'] = true;
---
+++ includes/cv.php
@@ -0,0 +1,110 @@
+<section id="image">
+  <div id="header_overlay"></div>
+</section>
+
+<section id="intro">
+  <img src="<?php echo $GLOBALS['url']; ?>res/images/face.jpeg" alt="Tom Marx, développeur sur Lyon" id="image_face">
+  <h2>Qui suis-je ?</h2>
+  <p>Je suis né en 2001 à Lyon, où j'ai passé toute ma (courte) vie. Je me suis très tôt intéressé aux nouvelles technologies et plus particulièrement à l'informatique. J'ai commencé en primaire, par de simples programmes en VB.net, pour ensuite découvrir l'HTML. Ma passion me suivit au collège et lycée, et j'appris des langages plus complexes, le PHP et le Java.
+Aujourd'hui, je réalise des sites internets, des applications mobiles, ou différents programmes. Je m'intéresse beaucoup au webdesign, et j'aime créer des interfaces, des logos ou encore des infographies. Je fais également de la musique électro, et je pratique le basket depuis plus de 10 ans.</p>
+</section>
+
+<section id="skills">
+  <div class="box">
+    <h2>Compétences</h2>
+    <div id="skills_list">
+      <div class="skill">
+        <h3><i class="fas fa-desktop"></i> Web</h3>
+        <p>Je maîtrise l'HTML, le CSS et le JS pour créer des sites de A à Z. Je peux utiliser des outils comme SASS, angular ou encore Bootstrap. Je me sers également de Git ce qui me permet de travailler de manière efficace avec un groupe. Je peux aussi intégrer des maquettes graphiques, et utiliser des CMS comme Wordpress ou CMSMadeSimple.</p>
+      </div>
+      <div class="skill">
+        <h3><i class="fas fa-gamepad"></i> Jeux vidéos</h3>
+        <p>Je crée des jeux vidéo multiplateformes. Je m'appuie principalement sur LibGDX et Unity. Je maîtrise les principales difficultés techniques à la création d'un jeu 2D, comme la gestion des ressources, la gestion du réseau, ou encore l'implémentation d'animations. Je connais également quelques bases théoriques en game design. J'ai participé à de nombreuses Game Jams, virtuelles et réelles.</p>
+      </div>
+      <div class="skill">
+        <h3><i class="fas fa-mobile-alt"></i> Applis mobiles</h3>
+        <p>Je peux développer des applis multiplateformes grâce au framework Ionic, et des applis Android natives. Je maîtrise le processus de déploiement, de la compilation à la livraison.</p>
+      </div>
+      <div class="skill">
+        <h3><i class="fas fa-pen-nib"></i> Design</h3>
+        <p>Je crée des maquettes de sites web à l'aide de Sketch. Je connais la retouche d'image avec Photoshop et je peux réaliser des dessins vectoriels simples.</p>
+      </div>
+      <div class="skill">
+        <h3><i class="fas fa-terminal"></i> Administration système</h3>
+        <p>Un terminal de commande m'est familier. Je maîtrise les principales fonctionnalités de Linux, comme la sécurisation des accès ou la gestion des dépendances. J'ai des notions de sécurité simples, comme l'escalade de privilège ou les dépassements de tampons.</p>
+      </div>
+      <div class="skill">
+        <h3><i class="fas fa-database"></i> Gestion de données</h3>
+        <p>Je peux traiter de larges ensembles de données automatiquement, les stocker en base. Je maîtrise la création de crawlers web en PHP ou NodeJS. J'ai développé des API assurant la sécurisation de mots de passe et la sécurisation de bases de données. Je connais bien MySQL, et les principales menaces qui le vise.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="experience">
+  <h2>Éxpérience</h2>
+  <div id="timeline">
+      <div class="timeline_left">
+        <span>Août 2018</span>
+        <div class="separator_down"></div>
+      </div>
+      <div class="timeline_right"><h3>Création de Basket Events</h3>
+        <p><a href="https://www.basket-events.com/" target="_blank">BasketEvents</a> est mon plus gros projet, alliant mes deux passions : l'informatique et le basket. Ce site rassemble tous les tournois de basket en France, et devrait à terme couvrir l'événementiel basket français. Ce site fonctionne en PHP, tout comme l'API qui gère les données du site. Un système d'inscription est en place, avec un envoi de mail, géolocalisation, affichage de marqueurs sur une carte leaflet... J'ai tout fait, de la maquette graphique à la gestion de la page Facebook.</p>
+      </div>
+
+      <div class="timeline_left"><span>Février 2018</span><div class="separator_down"></div></div>
+      <div class="timeline_right"><h3>Application pour Partir.com</h3>
+        <p>Développement d'une application listant tous les vols directs depuis ou à destination de l'aéroport de votre choix. Elle a été réalisée avec Ionic. J'ai créé un système d'autocomplétion, et géré le processus de livraison.
+        <br /><a class="button" target="_blank" href="https://play.google.com/store/apps/details?id=com.partir.volsdirects">Play Store<i class="fas fa-external-link-alt"></i></a>
+        <a class="button" target="_blank" href="https://itunes.apple.com/fr/app/vols-directs/id1291754547?mt=8">App store<i class="fas fa-external-link-alt"></i></a></p>
+      </div>
+
+
+      <div class="timeline_left"><span>Février 2017</span><div class="separator_down"></div></div>
+      <div class="timeline_right"><h3>Impact Game Jam</h3>
+        <p>Développement d'un jeu en équipe de 4 et en 48h sur le thème des inégalités sociales. Notre équipe a produit un jeu jouable et en phase avec le thème. Nous avons utilisé Unity.
+        <br /><a class="button" target="_blank" href="https://www.facebook.com/events/1641306709497617/">Lien de l'évenement<i class="fas fa-external-link-alt"></i></a>
+        </p>
+      </div>
+
+      <div class="timeline_left"><span>Décembre 2016</span><div class="separator_down"></div></div>
+      <div class="timeline_right"><h3>Ludum Dare #37</h3>
+        <p>Développement d'un jeu vidéo avec LibGDX en 48h lors de la plus grande Game Jam virtuelle internationale : le Ludum Dare. Le jeu a fini à la 704e position sur les 2400 entrées.
+        <br /><a class="button" target="_blank" href="http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=67826">Le jeu<i class="fas fa-external-link-alt"></i></a>
+        </p>
+      </div>
+
+      <div class="timeline_left"><span>Novembre 2016</span><div class="separator_down"></div></div>
+      <div class="timeline_right"><h3>Game Dev Party</h3>
+        <p>Développement d'un jeu vidéo en 72h lors de la Game Dev Party, organisée par l'association lyonnaise du même nom. Cet événement m'a permis de travailler dans une équipe de 7 personnes professionelles dans leurs domaines (graphisme, sound design, développement). Nous avons utilisé Unity, et ce fut une très bonne expérience de développement en équipe, j'ai pu étoffer mes connaissances de Git.
+        <br /><a class="button" target="_blank" href="http://gamedevparty.fr/game-jam-11-white-rabbit-rogue/">L'article<i class="fas fa-external-link-alt"></i></a>
+        </p>
+      </div>
+
+      <div class="timeline_left"><span>2016</span><div class="separator_down"></div></div>
+      <div class="timeline_right"><h3>Application pour Partir.com</h3>
+        <p>Développement d'une application porte-document pour le site de voyage Partir.com. J'ai utilisé le framework Ionic (1ère version). J'ai également dû développer une API en PHP qui permet d'importer des documents en envoyant des documents par mail. 3000 téléchargements en Septembre 2018.
+        <br /><a class="button" target="_blank" href="https://play.google.com/store/apps/details?id=com.partir.voyage&hl=fr">Play Store<i class="fas fa-external-link-alt"></i></a>
+        <a class="button" target="_blank" href="https://itunes.apple.com/us/app/porte-documents-et-checklist/id1146532159?mt=8">App store<i class="fas fa-external-link-alt"></i></a>
+        </p>
+      </div>
+
+      <div class="timeline_left"><span>Mai 2015</span><div class="separator_down"></div></div>
+      <div class="timeline_right"><h3>Stage en entreprise</h3>
+        <p>Stage d'une semaine dans l'entreprise BiMedia à La Roche-Sur-Yon dans le cadre d'un stage en 4ème, réalisation d'un programme NodeJS utilisé en interne. Ce programme affiche en temps réel les appels reçus par le call center du service client sur une carte de France. J'ai appris à documenter un programme et pris conscience de l'importance des protocoles qui entourent la création d'une solution technique au sein d'une entreprise de taille moyenne.</p>
+      </div>
+
+      <div class="timeline_left"><span>2015</span><div class="separator_down"></div></div>
+      <div class="timeline_right"><h3>Réalisation site recherche location</h3>
+        <p>Réalisation d'un site Internet style "AirBnB" listant les locations pour une ville et les affichant sur une carte. Ce site utilise Bootstrap et PHP.
+        <br /><a class="button" target="_blank" href="http://www.partir.com/comparateur-location/recherche.php5?sortBy=0&page=1&ville=Lyon%2C+France&lat=&long=&depart=16%2F12%2F2015&retour=26%2F12%2F2015&voyageurs=2&prix-min=0&prix-max=100">Lien<i class="fas fa-external-link-alt"></i></a></p>
+      </div>
+
+      <div class="timeline_left"><span>2014</span></div>
+      <div class="timeline_right"><h3>Programme d'alimentation de base de donnée</h3>
+        <p>Réalisation d'un programme PHP qui récupère des ventes privées depuis différentes sources de données et les met en base. Ce programme est exécuté tous les jours.
+        <br /><a class="button" target="_blank" href="https://www.partir.com/Etats-unis/promotions/">Lien<i class="fas fa-external-link-alt"></i></a></p>
+      </div>
+
+    </div>
+</section>
