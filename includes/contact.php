aeb0d719e1428418a15aaeec6915fc4c888a3892
Tom <tom@Toms-iMac.local>
2018-10-24 19:55:39 +0200

    Design contact

---
+++ includes/contact.php
@@ -0,0 +1,15 @@
+<section id="contact">
+
+  <div class="box" id="cadre">
+    <h1>Contactez-moi</h1>
+    <form id="contact_form">
+      <div id="first_row">
+        <input name="name" type='text' placeholder='Votre nom' />
+        <input name="mail" type='text' placeholder='Votre adresse mail' />
+      </div>
+      <textarea name="message" id="message" placeholder="Votre message"></textarea>
+      <div id="send_container"><button class="button" id="send" onclick='document.getElementById("contact_form").submit()'>Envoyer <i class="far fa-envelope"></i></button></div>
+    </form>
+  </div>
+
+</section>
