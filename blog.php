50ebf255050fe527d02b2b7abe716796f1f05564
Tom <tom@Toms-iMac.local>
2018-10-25 13:40:05 +0200

    Article IA partie 1 première version

--- blog.php
+++ blog.php
@@ -53,7 +53,7 @@ class Post
 
     public function get_preview()
     {
-        return substr($this->text_preview, 0, 200) . '...';
+        return substr($this->text_preview, 0, 200);
     }
 
     public function get_full_preview()
@@ -89,8 +89,8 @@ class Blog
     public function __construct()
     {
         $this->posts = [
-        new Post('Introduction aux réseaux de neurones - Partie 1', 'Nous verrons dans cet article à quoi sert un réseau de neurones, et comment ils fonctionnent. Dans cette première partie nous coderons un neurone et nous l\'entraineront à classer des fruits selon leurs caractéristiques.', 'ia-partie-1', ['IA'], '10/08/2018'),
-        new Post('Intelligence artificielle partie 2', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'ia-partie-2', ['IA'], '13/09/2018'),
+        new Post('Introduction aux réseaux de neurones - Partie 1 : Théorie', 'Cet article vous permettera de comprendre comment un neurone artificiel fonctionne, et comment le machine learning permet de créer des règles générales.', 'ia-partie-1', ['IA'], '10/08/2018'),
+        new Post('Introduction aux réseaux de neurones - Partie 2 : Mise en pratique', 'Maintenant que vous comprenez le fonctionnement d\'un neurone, nous allons coder une classe javascript capable de résoudre des problèmes.', 'ia-partie-2', ['IA'], '13/09/2018'),
         new Post('Mon expérience avec un Hackintosh', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'experience-hackintosh', ['Hackintosh'], '15/10/2018'),
         new Post('Ecole 42, qu\'est ce que c\'est ?', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '42-presentation', ['42'], '18/10/2018')
       ];
--- content/articles/ia-partie-1.php
+++ content/articles/ia-partie-1.php
@@ -1,26 +1,27 @@
 <script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML' async></script>
 <h3>Introduction</h3>
 <p>L'intelligence artificielle est un sujet en vogue. Une des branches les plus populaire de l'IA, les réseaux de neurones, sont présentés comme un <b>outil puissant</b> permettant de résoudre des problèmes jusqu'alors réservés au cerveau humain. Nous verrons ici leur cas d'utilisation et leur fonctionnement.
-Cette première partie est centrée sur le <b>fonctionnement d'un seul neurone</b>, qui constitue la base d'un réseau de neurone. Un neurone seul peut, nous le verrons, résoudre des problèmes simples. Après les explications théoriques je vous montrerai comment implémenter un neurone en Javascript, et nous résoudrons un premier problème grâce au <b>machine learning</b>.
-Cet article se veut destiné aux débutants, certains détails seront simplifiés afin de rendre les explications les plus claires possibles.</p>
-<h3>À quoi sert un réseau de neurone ?</h3>
-<p>Un réseau de neurone à un but précis : <b>prédire un résultat à partir de valeurs données</b>. Voici une liste d'exemple :
+Cette première partie est centrée sur le <b>fonctionnement théorique d'un seul neurone</b>, qui constitue la base d'un réseau de neurones. Un neurone seul peut, nous le verrons, résoudre des problèmes simples. Après les explications théoriques je vous montrerai dans la partie 2 comment implémenter un neurone en Javascript, et nous résoudrons un premier problème grâce au <b>machine learning</b>.
+<br />Nous utiliserons principalement des additions et des multiplications, aucune notion de maths avancés ou d'informatique n'est requise !
+Cet article se veut destiné aux débutants, certains détails seront simplifiés afin de rendre les explications les plus claires possible.</p>
+<h3>À quoi sert un réseau de neurones ?</h3>
+<p>Un réseau de neurones à un but précis : <b>prédire un résultat à partir de valeurs données</b>. Voici une liste d'exemple :
 <ul>
   <li>Prédire un résultat sportif à partir de facteurs comme la forme des joueurs, le classement des équipes</li>
-  <li>Décider les actions à exercer pour conduire une voiture autonome grâce aux données des capteurs retranscrivants l'environment</li>
+  <li>Décider les actions à exercer pour conduire une voiture autonome grâce aux données des capteurs retranscrivants l'environnement</li>
   <li>Déterminer si un mail est un spam en se basant sur des paramètres comme la forme de l'adresse d'envoi, la longueur du mail, la présence de certains mot-clés</li>
   <li>Détecter une maladie en se basant sur des critères comme l'activité de la personne, son alimentation</li>
 </ul>
-Dans touts ces cas, le réseau prend en entrée des données, et nous sort une nouvelle valeure.
+Dans touts ces cas, le réseau prend en entrée des données, et sort une nouvelle valeur.
 </p>
 
 <p>Mais comment le réseau peut-il "deviner" la valeur attendue ? C'est là qu'intervient le <b>machine learning</b>. Il faut entrainer le réseau avec des milliers voir des millions de cas déjà résolus.<br />
-Reprenons l'exemple du spam : votre messagerie doit détecter si un mail reçu est un spam ou non. Il va constituer une base de données de mails spam signalés par les utilisateurs. Grâce à cette base de mails spam il peut entrainer son réseau de neurone à détecter de nouveaux cas inconnus. Un réseau de neurones crée une règle générale à partir de données existantes afin de pouvoir appliquer cette règle à de nouveaux cas.</p>
+Reprenons l'exemple du spam : votre messagerie doit détecter si un mail reçu est un spam ou non. Il va constituer une base de données de mails spam signalés par les utilisateurs. Grâce à cette base de mails spam il peut entrainer son réseau de neurones à détecter de nouveaux cas inconnus. <b>Un réseau de neurones crée une règle générale à partir de données existantes afin de pouvoir appliquer cette règle à de nouveaux cas</b>.</p>
 
 <h3>Comment fonctionne un neurone ?</h3>
 <p>Nous rentrons maintenant dans la partie technique. Un neurone virtuel est une imitation très simplifiée d'un neurone biologique. Ce neurone prend en entrée une liste de valeurs. Il fait un cacul que nous détaillerons plus tard et retourne une valeur de sortie.<br />
-À chaque entrée correspond un <b>poids</b>. Le poids est un coefficient qui indique quelle est <b>l'importance d'une entrée sur le résultat</b>. Quelle valeur ces poids prennent t-ils ? Ils sont calculés grâce à l'entrainement du neurone. La seule fonction de l'entrainement est de trouver des poids qui donnent le résultat le plus correcte possible.
-Imaginez une boite avec des potars qui correspondent au poids. Le machine learning c'est tourner ces boutons dans touts les sens pour trouver une configuration qui donne le résultat le plus satisfaisant possible.<br/>
+À chaque entrée correspond un <b>poids</b>. Le poids est un coefficient qui indique quelle est <b>l'influence d'une entrée sur le résultat</b>. Quelle valeur ces poids prennent t-ils ? Ils sont calculés grâce à l'entrainement du neurone. La seule fonction de l'entrainement est de trouver des poids qui donnent le résultat le plus correcte possible.
+Imaginez une boite avec des potars qui correspondent au poids. Le machine learning c'est tourner ces boutons dans tous les sens pour trouver une configuration qui donne le résultat le plus satisfaisant possible.<br/>
 Prenons un exemple : notre neurone a pour but de prédire si un patient est malade ou non. Il renvoi 1 si le patient est malade, 0 si il est sain. Ses entrées sont :
 <ul>
   <li>Le patient fait-il du sport ?</li>
@@ -44,7 +45,7 @@ Toutes ces entrées sont binaires, soit 1, soit 0. On entraine le neurone avec u
     <br />
     `S = sum(Wi * X i)`
     <br />
-    On ajoute ensuite le bias, une nouvelle entrée dont vous comprendrez l'utilitée lors de la mise en pratique plus bas. C'est une valeur qui a le même rôle qu'une entrée classique, elle nous permettera de résoudre certains problèmes.
+    On ajoute ensuite le <b>bias</b>, noté `\beta`, une nouvelle valeur qui ne dépend d'aucune entrée dont vous comprendrez l'utilitée lors de la mise en pratique. C'est une valeur qui a le même rôle qu'une entrée classique, elle nous permettera de résoudre certains problèmes.
     <br />
     `S = sum(Wi * X i) + \beta`
   </li>
@@ -70,4 +71,91 @@ Résumons toutes les grandeurs composants un neurone :
   <li>Un bias `\beta`</li>
   <li>Une fonction d'activation, généralement sigmoïd ou signe</li>
 </ul>
+</p>
+<h3>L'apprentissage du neurone</h3>
+<p>Nous allons maintenant aborder une partie cruciale dans le fonctionnement d'un réseau de neurones : l'apprentissage. Le machine learning consiste à donner une grande quantité d'exemples issus d'un <b>dataset</b>, un large ensemble de données, afin de trouver les poids qui donne un résultat le plus juste possible. Autrement dit, <b>il faut minimiser l'erreur d'un neurone</b>. Pour chaque donnée d'entrainement, le neurone va ajuster ses poids pour se rapprocher du résultat attendu. Voici le procédé d'apprentissage :
+<ul>
+  <li>
+    On donne au neurone une liste d'entrée, et la sortie attendue. Pour reprendre notre exemple précédent, on a 3 informations : le patient fait-il du sport, fume t-il, et possède t-il une trotinette. On a également la sortie attendue, le patient est sain ou malade. On a toutes les informations, on va maintenant utiliser ces données pour corriger les poids.
+  </li>
+  <li>
+    En utilisant les calculs précédents, on prédit le résultat (malade ou sain) en se basant sur les entrées. On a maintenant notre prédiction et la véritable réponse. On peut calculer l'erreur :<br / />
+    `E = "réponse" - "prédiction"`<br />
+    Si nous avions prédit le bon résultat, l'erreur serait égale à 0. Sinon, l'erreur peut-être 1 ou -1.
+  </li>
+  <li>
+    Avec cette erreur, on est capable de corriger les poids des neurones, grâce à la <b>régression linéaire</b>. Ce procédé mathématique consiste à ajouter au poids son entrée multipliée par l'erreur. En effet, plus l'erreur est grande, plus la correction à effectuer est grande. Si le neurone a juste, alors l'erreur vaut 0 et aucune correction n'est effectuée sur le poids.
+    <br />
+    `Wi = Wi + "Xi" * E`
+    <br />
+    On ne peut malheureusement pas s'arréter à ce calcul, nous avons besoin de diminuer la correction apportée : je vous avais dit que le machine learning consistait à tourner des boutons jusqu'à ce qu'il nous donne un résultat convaincant. Ici nos boutons ce sont les poids, qu'il faut ajuster pour trouver les prédictions les plus justes possible. Imaginez qu'à chaque erreur vous tourniez le bouton d'un demi-tour. Vous n'arriverez jamais au bon réglage car vous manqueriez de précision. Ici c'est pareil, on multiplie la correction par un <b>learning rate</b>, qu'on définira ici à 0,1. Le learning rate c'est la précision avec laquelle vous ajustez vos poids.<br />
+    `Wi = Wi + "Xi" * E * "learning rate"`<br />
+    Il est important de préciser qu'il existe différents type de régressions, la plus simple étant la linéaire mais il en existe d'autres comme la régression logistique. Dans notre cas la régression linéaire nous suffira largement.
+  </li>
+  <li>
+    Maintenant que nos poids sont corrigés, on corrige le bias, la valeur supplémentaire, de la même manière que les poids. Seulement il n'est associé à aucune entrée, donc on lui ajoute seulement l'erreur multipliée par le learning rate :
+    <br />
+    `\beta = \beta + E * "learning rate"`
+  </li>
+</ul>
+On peut résumer ce procédé avec ce schéma :
+<div class="full_width">
+  <img src="<?php echo $GLOBALS['url']; ?>content/articles/ia-partie-1/schema2.jpg" alt="Schéma machine learning"/>
+</div>
+On voit ici que :
+<ul>
+  <li>
+    Le poids sport n'a pas été amélioré. Il serait amélioré dans le cas où nous pronosticons la maladie mais le malade est sain. L'erreur serait alors -1, et le poids serait égal à 2,2.
+  </li>
+  <li>
+    Le poids cigarette a été amélioré. Sa valeur est passée de -0,3 à -0,2. Nous avons estimé que ce poids devrait avoir une valeur positive, donc il se rapproche de cet objectif.
+  </li>
+  <li>
+    Le poids trotinette a regressé : sa valeur à augmentée, alors qu'elle devrait se rapprocher de 0. Nous avons vu que le rôle d'un neurone était de créer une règle générale. Ici sa seule information est que le patient est malade et qu'il possède une trotinette. Il est logique que le neurone crée une corrélation. Cependant, en répétant l'opération avec l'ensemble du dataset, le neurone va faire rapprocher ce poids de 0.
+  </li>
+  <li>
+    Le bias a augmenté sa valeur, mais on ne sait pas forcemment vers quoi il doit tendre.
+  </li>
+</ul>
+On en déduit qu'il faut un très grand nombre de répétitions afin de trouver les poids optimums. Il faut également un dataset qui offre beaucoup de combinaisons différentes.
+</p>
+<h3>Conclusion</h3>
+<p>
+  Résumons ce que nous avons vu dans cette première partie :
+  <ul>
+    <li>
+      Un neurone prend des entrées et réalise une prédiction à partir de ces entrées.
+    </li>
+    <li>
+      À chaque entrée correspond un poids qui défini l'influence de l'entrée sur le résultat.
+    </li>
+    <li>
+      Le machine learning ou l'entrainement vise à chercher les poids qui réduisent l'erreur du neurone au maximum.
+    </li>
+    <li>
+      Le dataset doit être le plus grand et le plus varié possible, avec un nombre minimal d'erreurs.
+    </li>
+  </ul>
+  Un neurone est composé :
+  <ul>
+    <li>
+      D'une liste de poids qui correspondent à chaque entrée.
+    </li>
+    <li>
+      D'une valeur qui ne dépend d'aucune entrée, le bias.
+    </li>
+    <li>
+      D'une fonction d'activation, généralement sigmoïd ou signe, qui normalise une somme entre 0 et 1.
+    </li>
+    <li>
+      D'une fonction de prédiction, qui correspond à la somme des entrées multipliées par les poids correspondants plus le bias, tout ça passé dans la fonction d'activation.
+    </li>
+    <li>
+      D'une fonction d'entrainement, qui prend des entrées et la sortie associée. Elle réalise une prédiction, calcule l'erreur entre la prédiction et la solution, puis corrige chacun des poids et le bias grâce à un procédé mathématique appelé "régression linéaire".
+    </li>
+  </ul>
+  Dans la partie 2, nous allons implémenter cette théorie en code, et résoudre notre premier problème.
+  <div class="full_width">
+    <a href="<?php echo $blog->get_post_by_link('ia-partie-2')->get_url(); ?>" class="button">Partie 2</a>
+  </div>
 </p>
