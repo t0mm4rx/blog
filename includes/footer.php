da65fbfadaab10a0864af0784d894656b1697e1d
t0mm4rx <tom.marx@hotmail.fr>
2019-06-22 19:40:22 +0200

    Twitter footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 <div class="links">
   <div class="link_social"><a href="https://github.com/t0mm4rx" target="_blank"><i class="fab fa-github"></i></a></div>
   <div class="link_social"><a href="https://t0m.itch.io/" target="_blank"><i class="fas fa-gamepad"></i></a></div>
-  <div class="link_social"><a href="https://dribbble.com/JumpingTom" target="_blank"><i class="fab fa-dribbble"></i></a></div>
+  <div class="link_social"><a href="https://twitter.com/AirM4rx" target="_blank"><i class="fab fa-twitter"></i></a></div>
   <div class="link_social"><a href="https://codepen.io/t0mm4rx/" target="_blank"><i class="fab fa-codepen"></i></a></div>
 </div>
 <p>Copyright Tom Marx - 2019</p>
