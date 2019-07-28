f042779fcec7b1e5b53a6b07a5db54994181191c
t0mm4rx <tom.marx@hotmail.fr>
2019-07-28 08:29:32 +0200

    CV header Partir

--- includes/cv.php
+++ includes/cv.php
@@ -52,6 +52,14 @@ Aujourd'hui, je réalise des sites internets, des applications mobiles, ou diff�
         <span>Juin 2019</span>
         <div class="separator_down"></div>
       </div>
+      <div class="timeline_right"><h3>Réalisation header mobile de Partir.com</h3>
+        <p>J'ai réalisé la navigation mobile du site Partir.com. Ce site reçoit 1 million de visiteurs par mois dont 40% de visiteurs mobiles.</p>
+      </div>
+
+      <div class="timeline_left">
+        <span>Juin 2019</span>
+        <div class="separator_down"></div>
+      </div>
       <div class="timeline_right"><h3>Finaliste Prologin</h3>
         <p>Après avoir passé les qualifications en ligne et les demi-finales, je termine 50ème sur 100 participants de la finale nationale de Prologin. Prologin est les premier concours d'informatique en France, organisé par Epita et Polytechnique. La finale consiste à créer une intelligence artificielle qui joue à un jeu crée pour l'occasion. Un tournoi faisant jouer les IA les unes contre les autres et ensuite organisé, et permet de déterminer un classement individuel.</p>
       </div>
